430517024922a51ca0ee337e468844a6120be357
MO-Huang <1318325359@qq.com>
2025-08-06 12:39:03 +0800

    添加是否切换的标志，切换标志、切换周期和初始位置通过参数获取，进行相关内容修改

--- Star-Searcher/gazebo_map_generator/src/DoorControlPlugin.cc
+++ Star-Searcher/gazebo_map_generator/src/DoorControlPlugin.cc
@@ -3,6 +3,7 @@
 #include <gazebo/common/common.hh>
 #include <iostream>
 #include <chrono>
+#include <ostream>
 
 namespace gazebo
 {
@@ -20,7 +21,7 @@ namespace gazebo
       }
 
       // 加载插件
-      void Load(physics::ModelPtr _model, sdf::ElementPtr /*_sdf*/)
+      void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
       {
         // 存储模型指针
         this->model = _model;
@@ -41,12 +42,29 @@ namespace gazebo
         this->updateConnection = event::Events::ConnectWorldUpdateBegin(
             std::bind(&WallAndDoorPlugin::OnUpdate, this));
         
-        // 设置切换周期时间
+        // 获取是否切换的标志和切换周期
         this->lastSwitchTime = std::chrono::steady_clock::now();
-        this->switchDuration = std::chrono::seconds((rand() % 41) + 20);
+        switchFlag = true;
+        if (_sdf->HasElement("switch_flag")) {
+          switchFlag = _sdf->Get<bool>("switch_flag");
+          std::cout << "\033[32minitial_position set to " << switchFlag <<".\033[0m" << std::endl;
+        }
+        // this->switchDuration = std::chrono::seconds((rand() % 11) + 15);
         // this->switchDuration = std::chrono::seconds(85);
+        if (_sdf->HasElement("switch_duration")) {
+          double sec = _sdf->Get<double>("switch_duration");
+          this->switchDuration = std::chrono::seconds(static_cast<int>(sec));
+          printf("\033[32mswitch_duration set to %fs.\033[0m\n", sec);
+        } else {
+          this->switchDuration = std::chrono::seconds((rand() % 11) + 15);
+        }
 
-        this->targetPos = 0;
+        double initPos = 0.0;
+        if (_sdf->HasElement("initial_position")) {
+          initPos = _sdf->Get<double>("initial_position");
+          printf("\033[32minitial_position set to %frad.\033[0m\n", initPos);
+        }
+        this->targetPos = initPos;
 
         this->model->GetJointController()->SetPositionTarget(this->joint->GetScopedName(), this->targetPos);
       }
@@ -55,25 +73,27 @@ namespace gazebo
       void OnUpdate()
       {
         this->currentAngle = this->joint->Position(0);
-        if(abs(this->currentAngle - this->targetPos) > 0.01)
-          printf("\033[34m currentAngle = %f\033[0m\n", this->currentAngle);
+        // if(abs(this->currentAngle - this->targetPos) > 0.01)
+        //   printf("\033[34m currentAngle = %f\033[0m\n", this->currentAngle);
 
         // 获取当前时间
         auto now = std::chrono::steady_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::seconds>(now - this->lastSwitchTime);
 
-        // 如果超过设定的切换时间，就切换速度方向
-        if (duration >= this->switchDuration)
-        {
-          if(this->targetPos == 1.57)
-            this->targetPos = 0.0;
-          else
-            this->targetPos = 1.57;
-          this->model->GetJointController()->SetPositionTarget(this->joint->GetScopedName(), this->targetPos);
-          std::cout << "切换门的目标位置: " << this->targetPos << std::endl;
+        if(switchFlag) {
+          // 如果超过设定的切换时间，就切换速度方向
+          if (duration >= this->switchDuration)
+          {
+            if(this->targetPos == 1.57)
+              this->targetPos = 0.0;
+            else
+              this->targetPos = 1.57;
+            this->model->GetJointController()->SetPositionTarget(this->joint->GetScopedName(), this->targetPos);
+            // std::cout << "切换门的目标位置: " << this->targetPos << std::endl;
 
-          // 更新上次切换时间
-          this->lastSwitchTime = now;
+            // 更新上次切换时间
+            this->lastSwitchTime = now;
+          }
         }
       }
 
@@ -85,6 +105,8 @@ namespace gazebo
       
       double targetPos;
       double currentAngle;
+
+      bool switchFlag;
 
       // 切换控制的时间
       std::chrono::steady_clock::time_point lastSwitchTime;
